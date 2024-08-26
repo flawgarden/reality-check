70ab5136dffff6bfdbf419277600407505db69f2
Aleksandr Misonizhnik <misonijnik@gmail.com>
2024-08-26 15:25:59 +0200

    fix:

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,9 +11,27 @@ requireCommand() {
 requireCommand git
 requireCommand python3
 
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+EXIT_ON_ERROR="false"
+BOOTSTRAP_OPTIONS=""
+
+for OPT in "$@"; do
+  if [[ "$OPT" = *"--exit-on-error"* ]]; then
+      EXIT_ON_ERROR="true"
+      BOOTSTRAP_OPTIONS="$BOOTSTRAP_OPTIONS --exit-on-error"
+      shift 1
+  fi
+  if [[ "$OPT" = *"--update"* ]]; then
+      BOOTSTRAP_OPTIONS="$BOOTSTRAP_OPTIONS --update"
+      shift 1
+  fi
+done
+
+if [[ "$EXIT_ON_ERROR" = "true" ]]; then
+  set -e
+fi
+
 cd "$SCRIPT_DIR" || exit
-./java/bootstrap.sh
-./csharp/bootstrap.sh
+./java/bootstrap.sh "$BOOTSTRAP_OPTIONS"
+./csharp/bootstrap.sh "$BOOTSTRAP_OPTIONS"
