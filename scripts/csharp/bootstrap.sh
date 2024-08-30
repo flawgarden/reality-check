bc3b0e0f16030d1fda1e066cfb3a9f544180edc4
Aleksandr Misonizhnik <misonijnik@gmail.com>
2024-08-30 09:52:16 +0000

    feat: Add initial C# CVE Benchmark

--- scripts/csharp/bootstrap.sh
+++ scripts/csharp/bootstrap.sh
@@ -14,12 +14,10 @@ requireCommand python3
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 EXIT_ON_ERROR="false"
-BOOTSTRAP_OPTIONS=""
 
 for OPT in "$@"; do
   if [[ "$OPT" = *"--exit-on-error"* ]]; then
       EXIT_ON_ERROR="true"
-      BOOTSTRAP_OPTIONS="$BOOTSTRAP_OPTIONS --exit-on-error"
       shift 1
   fi
 done
@@ -29,5 +27,5 @@ if [[ "$EXIT_ON_ERROR" = "true" ]]; then
 fi
 
 cd "$SCRIPT_DIR" || exit
-./bootstrap.sh "java" "$BOOTSTRAP_OPTIONS"
-./bootstrap.sh "csharp" "$BOOTSTRAP_OPTIONS"
+../collect_benchmark.py -l=csharp -db=cves_db.csv
+../markup_benchmark.py -l=csharp
